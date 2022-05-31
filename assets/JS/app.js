b72904f53f9e8c1dcb5a64273b0627b521082095
brunojuwer <bruno.juwer1997@gmail.com>
2022-05-31 15:55:13 -0300

    Código do relógio

---
+++ assets/JS/app.js
@@ -0,0 +1,14 @@
+const clockContainer = document.querySelector('.clock-container');
+
+const updateClock = () => {
+    const present = new Date();
+    const hour = present.getHours();
+    const minutes = present.getMinutes();
+    const seconds = present.getSeconds();
+
+    const addTime = time => `<span>${String(time).length === 1 ? "0" + time : time}</span>`;
+
+    clockContainer.innerHTML = `${addTime(hour)} : ${addTime(minutes)} : ${addTime(seconds)} `
+}
+
+setInterval(updateClock, 1000);
